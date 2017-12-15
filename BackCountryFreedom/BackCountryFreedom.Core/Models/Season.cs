3ee719a0c648aea97d2c4445e1fcb26149d1f667
William E Morgan <bmorgan@telusplanet.net>
2017-12-15 10:58:14 -0700

    incremental

--- BackCountryFreedom/BackCountryFreedom.Core/Models/Season.cs
+++ BackCountryFreedom/BackCountryFreedom.Core/Models/Season.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using BackCountryFreedom.Core.Models;
+using System.ComponentModel;
 
 namespace BackCountryFreedom.Core.Models
 {
@@ -11,6 +12,7 @@ namespace BackCountryFreedom.Core.Models
     {
         [Required]
         [StringLength (100)]
+        [DisplayName("Season")]
         public String Description { get; set; }
     }
 }
